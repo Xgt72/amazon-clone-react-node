c2797cdf2dfe75948f825c3e69737d5977aa11cf
Xavier Georget <xageorget@gmail.com>
2021-05-05 21:21:21 +0200

    header with search bar, navbar and logo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./components/Header";
+
+import './App.css';
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Hello Amazon Clone App 🚀</h1>
+      <Header />
+      {/* Home */}
+    </main>
+  );
+}
+
+export default App;
