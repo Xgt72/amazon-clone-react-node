5b2678e5ab80740ee8429f4c37559b5a00607787
Xavier Georget <xageorget@gmail.com>
2021-05-23 21:54:14 +0200

    orders and baskets routes, controllers, models

--- back/routes/users.routes.js
+++ back/routes/users.routes.js
@@ -1,19 +1,29 @@
 const usersRouter = require("express").Router();
 const {
-    getAllUsers,
-    getOneUserById,
-    getOneUserByEmail,
-    createOneUser,
-    updateOneUser,
-    deleteOneUser,
-    passwordIsValid,
+  getAllUsers,
+  getOneUserById,
+  getOneUserByEmail,
+  createOneUser,
+  updateOneUser,
+  deleteOneUser,
+  passwordIsValid,
 } = require("../controllers/users");
-const { createToken, authenticateWithJsonWebToken } = require("../controllers/jwt");
+const { getAllOrdersByUserId } = require("../controllers/orders");
+const {
+  createToken,
+  authenticateWithJsonWebToken,
+} = require("../controllers/jwt");
 
 usersRouter.get("/", getAllUsers);
 usersRouter.post("/register", createOneUser, getOneUserById);
 usersRouter.post("/login", getOneUserByEmail, passwordIsValid, createToken);
-usersRouter.put("/:id", authenticateWithJsonWebToken, updateOneUser, getOneUserById);
+usersRouter.put(
+  "/:id",
+  authenticateWithJsonWebToken,
+  updateOneUser,
+  getOneUserById
+);
 usersRouter.delete("/:id", authenticateWithJsonWebToken, deleteOneUser);
+usersRouter.get("/:id/orders", getAllOrdersByUserId);
 
 module.exports = usersRouter;
